7321ab47e3dbde54b519ae153fd0accb1a24a800
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-10-20 18:33:45 +0300

    Update 2020-10-20T15:33:44.372Z

--- js/app.js
+++ js/app.js
@@ -1,2 +1,22 @@
+const btn = document.querySelector('.button-subscribe');
+
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+// })
+const slider = document.querySelector('.slider-row');
+const slides = document.querySelectorAll('.slider-item');
+const prev = document.querySelector('.slider-button-prev');
+const next = document.querySelector('.slider-button-next');
 
 
+next.addEventListener('click', () => {
+    slides.forEach(slide => {
+        slide.setAttribute('style', 'transform: translate3d(-984px, 0, 0)')
+    })
+})
+
+prev.addEventListener('click', () => {
+    slides.forEach(slide => {
+        slide.setAttribute('style', 'transform: translate3d(984px, 0, 0)')
+    })
+})
