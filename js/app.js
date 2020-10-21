d5162f01fc976b696ebfcbf8f388196227978de4
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-10-21 14:30:31 +0300

    Update 2020-10-21T11:30:29.966Z

--- js/app.js
+++ js/app.js
@@ -1,8 +1,42 @@
+const formWrapper = document.querySelector('.subscribe-wrapper');
 const btn = document.querySelector('.button-subscribe');
+const template = 
+`<h2 class="main-title">
+    Узнайте о запуске сервиса первым
+</h2>
+<div class="subscribe-ok">
+    <img src="./images/ok.svg" alt="вы подписаны">
+    <p>
+        Спасибо! Мы будем держать вас в курсе обновлений
+    </p>
+</div>
+`
 
-// btn.addEventListener('click', (e) => {
-//     e.preventDefault();
-// })
+const subscribeForm = document.querySelector('form');
+
+
+
+function isFormValid(form) {
+    const inputs = [form.querySelector('input')];
+    let valid = true;
+    inputs.forEach((input) => {
+        if (input.type !== 'submit' && input.type !== 'button') {
+            if (!input.checkValidity()) {
+                valid = false;
+            }
+        }
+    });
+    return valid
+}
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if ( isFormValid(subscribeForm) ) {
+        formWrapper.textContent = '';
+        formWrapper.insertAdjacentHTML('beforeend', template);
+    }
+})
 const slider = document.querySelector('.slider-row');
 const slides = document.querySelectorAll('.slider-item');
 const prev = document.querySelector('.slider-button-prev');
@@ -20,3 +54,4 @@ prev.addEventListener('click', () => {
         slide.setAttribute('style', 'transform: translate3d(984px, 0, 0)')
     })
 })
+
