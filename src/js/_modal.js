0dd576bf77f2d2651298f8d984bd5f961d3cba09
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-10-21 14:30:15 +0300

    modal window done

--- src/js/_modal.js
+++ src/js/_modal.js
@@ -1,5 +1,39 @@
+const formWrapper = document.querySelector('.subscribe-wrapper');
 const btn = document.querySelector('.button-subscribe');
+const template = 
+`<h2 class="main-title">
+    Узнайте о запуске сервиса первым
+</h2>
+<div class="subscribe-ok">
+    <img src="./images/ok.svg" alt="вы подписаны">
+    <p>
+        Спасибо! Мы будем держать вас в курсе обновлений
+    </p>
+</div>
+`
 
-// btn.addEventListener('click', (e) => {
-//     e.preventDefault();
-// })
+const subscribeForm = document.querySelector('form');
+
+
+
+function isFormValid(form) {
+    const inputs = [form.querySelector('input')];
+    let valid = true;
+    inputs.forEach((input) => {
+        if (input.type !== 'submit' && input.type !== 'button') {
+            if (!input.checkValidity()) {
+                valid = false;
+            }
+        }
+    });
+    return valid
+}
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if ( isFormValid(subscribeForm) ) {
+        formWrapper.textContent = '';
+        formWrapper.insertAdjacentHTML('beforeend', template);
+    }
+})
