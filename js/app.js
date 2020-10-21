a21e30bcaa49bf7b9c8070038fdba6e041752f7a
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-10-21 18:45:15 +0300

    Update 2020-10-21T15:45:13.771Z

--- js/app.js
+++ js/app.js
@@ -42,16 +42,31 @@ const slides = document.querySelectorAll('.slider-item');
 const prev = document.querySelector('.slider-button-prev');
 const next = document.querySelector('.slider-button-next');
 
+let state = 0;
+let step = slides[0].clientWidth;
 
 next.addEventListener('click', () => {
     slides.forEach(slide => {
-        slide.setAttribute('style', 'transform: translate3d(-984px, 0, 0)')
+        slide.setAttribute('style', `transform: translate3d(${ state - step }px, 0, 0)`);
     })
+    state = state - step;
+    
+    if ( state < 0 ) {
+        next.setAttribute('disabled', '');
+        prev.removeAttribute('disabled', '');
+    }
 })
+
 
 prev.addEventListener('click', () => {
     slides.forEach(slide => {
-        slide.setAttribute('style', 'transform: translate3d(984px, 0, 0)')
+        slide.setAttribute('style', `transform: translate3d(${ state + step }px, 0, 0)`);
     })
+    state += step;
+    
+    if ( state > 0 ) {
+        prev.setAttribute('disabled', '');
+        next.removeAttribute('disabled', '');
+    }
 })
 
